37f6eb09b831ebeb6987660cdeb37636c2a779d5
adrian-lucas <donplucas@gmail.com>
2024-06-15 18:49:47 -0400

    Commit Forzado

--- frontend-eva/src/pages/CreateExercise.jsx
+++ frontend-eva/src/pages/CreateExercise.jsx
@@ -1,36 +1,43 @@
 import TextEditor from "../components/TextEditor"
 import TerminalPyodide from "./TerminalPyodide"
-import { TextareaAutosize,Container,Typography,Stack,Box } from "@mui/material"
+import { TextareaAutosize,Container,Typography,Stack,Box,Button } from "@mui/material"
 function CreateExercise() {
   return (
     <Container>
-      <Stack spacing={2}>
          <Typography variant="h4">
             Crear Ejercicio
          </Typography>
+      <Stack spacing={2} direction={'row'}>
          <Box>
             <Typography variant="h5">
                Descripcion
             </Typography>
-            <TextEditor />
+            {/* <Box width={'70%'}> */}
+               <TextEditor />
+            {/* </Box> */}
          </Box>
-         <Box>
-            <Typography variant="h5">
-               Ejercicio
-            </Typography>
-            <TerminalPyodide/>
-         </Box>
-         <Box>
-            <Typography variant="h5">
-               Vista  del Estudiante
-            </Typography>
-            <TextareaAutosize style={{
-               width: "100%",
-               height: "200px",
-               fontFamily: "monospace",
-               fontSize: "1rem",
-            }} />
-         </Box>
+         <Stack spacing={2}>
+            <Box>
+               <Typography variant="h5">
+                  Ejercicio
+               </Typography>
+               <TerminalPyodide/>
+            </Box>
+            <Box>
+               <Typography variant="h5">
+                  Vista  del Estudiante
+               </Typography>
+               <TextareaAutosize style={{
+                  width: "100%",
+                  height: "200px",
+                  fontFamily: "monospace",
+                  fontSize: "1rem",
+               }} />
+            </Box>
+         <Button variant="contained">
+            Registrar
+         </Button>
+         </Stack>
       </Stack>
     </Container>
   )
