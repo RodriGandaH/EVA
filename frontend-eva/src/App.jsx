363cc698e9ca5ef6e8fc714edfb7b1a462f15d21
adrian-lucas <donplucas@gmail.com>
2024-06-19 18:34:07 -0400

    instalacion libreria scorm

--- frontend-eva/src/App.jsx
+++ frontend-eva/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
 import Router from '../routes/Router';
-
-
 import { BrowserRouter  } from 'react-router-dom';
+import ScormProvider from 'react-scorm-provider';
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Router/>
-            </BrowserRouter>
+            <ScormProvider version="1.2" debug={process.env.NODE_ENV !== 'production'}>
+                <BrowserRouter>
+                    <Router/>
+                </BrowserRouter>
+            </ScormProvider>
         </>
     );
 }
