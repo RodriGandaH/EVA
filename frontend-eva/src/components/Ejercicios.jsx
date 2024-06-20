2c8fcdb399e2463f13f895a2f844a97c262a8bd0
marcelo <marcheloortuo@gmail.com>
2024-06-20 16:42:25 -0400

    añadido de ayudas v2

--- frontend-eva/src/components/Ejercicios.jsx
+++ frontend-eva/src/components/Ejercicios.jsx
@@ -3,10 +3,11 @@ import { jsPython } from 'jspython-interpreter';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
-import { Container, Grid, Typography, Button, TextField,Stack,IconButton,CircularProgress ,Box} from '@mui/material';
+import { Container, Grid, Typography, Button, TextField, Stack, IconButton, CircularProgress, Box } from '@mui/material';
 import TerminalPyodide from '../pages/TerminalPyodide';
 import { useNavigate } from 'react-router-dom';
 import { GoHomeFill } from "react-icons/go";
+
 function Ejercicios() {
     const [pyprompt, setPyprompt] = useState('');
     const [pyoutput, setPyoutput] = useState(null);
@@ -19,21 +20,41 @@ function Ejercicios() {
     const [isError, setIsError] = useState(false);
     const [resultadoDocente, setResultadoDocente] = useState('');
     const [puedeContinuar, setPuedeContinuar] = useState(false);
+    const [intentosFallidos, setIntentosFallidos] = useState(0); // Estado para contar los intentos fallidos
+
+    // Función para procesar el código del estudiante y ocultar ayudas según los intentos fallidos
+    const procesarCodigoEstudiante = (codigo, intentos) => {
+        let codigoProcesado = codigo;
+        if (intentos < 3) {
+            codigoProcesado = codigo.replace(/#ayuda 1.*$/gm, '').replace(/#ayuda 2.*$/gm, '');
+        } else if (intentos >= 3 && intentos < 5) {
+            codigoProcesado = codigo.replace(/#ayuda 2.*$/gm, '');
+        }
+        setCodigoEstudiante(codigoProcesado);
+    };
 
     const obtenerEjercicios = async () => {
         setFlag(true);
         const response = await fetch('http://localhost:8000/api/ejercicios');
         const data = await response.json();
         setEjercicios(data);
-        setPyprompt(data[0].codigo_estudiante);
+        setEjercicioActual(0); // Iniciar desde el primer ejercicio
+        setPyprompt(procesarCodigoInicial(data[0].codigo_estudiante));
+        setCodigoEstudiante(procesarCodigoInicial(data[0].codigo_estudiante));
     };
+
+    const procesarCodigoInicial = (codigo) => {
+        // Función para limpiar el código inicial de ayudas
+        return codigo.replace(/#ayuda 1.*$/gm, '').replace(/#ayuda 2.*$/gm, '');
+    };
+
     useEffect(() => {
         obtenerEjercicios();
     }, []);
+
     useEffect(() => {
-      setFlag(false);
-    }, [ejercicios])
-    
+        setFlag(false);
+    }, [ejercicios]);
 
     useEffect(() => {
         console.log('Resultado Estudiante', resultadoEstudiante);
@@ -55,8 +76,7 @@ function Ejercicios() {
     };
 
     const runScriptDocente = async () => {
-        const fullScript =
-            ejercicios[ejercicioActual].codigo_docente + '\nprint(res)';
+        const fullScript = ejercicios[ejercicioActual].codigo_docente + '\nprint(res)';
         try {
             const result = await jsPython().evaluate(fullScript);
             setResultadoDocente(result);
@@ -73,23 +93,28 @@ function Ejercicios() {
 
         if (resultadoEstudiante === resultadoDocente) {
             setPuedeContinuar(true);
+            setIntentosFallidos(0); // Reiniciar intentos fallidos si es correcto
+        } else {
+            // Incrementar intentos fallidos y procesar el código del estudiante
+            const nuevosIntentosFallidos = intentosFallidos + 1;
+            setIntentosFallidos(nuevosIntentosFallidos);
+            procesarCodigoEstudiante(ejercicios[ejercicioActual].codigo_estudiante, nuevosIntentosFallidos);
         }
     };
 
     const handleSiguiente = () => {
         const nextEjercicio = ejercicioActual + 1;
         setEjercicioActual(nextEjercicio);
-        setCodigoEstudiante(ejercicios[nextEjercicio].codigo_estudiante);
+        setPyprompt(procesarCodigoInicial(ejercicios[nextEjercicio].codigo_estudiante));
+        setCodigoEstudiante(procesarCodigoInicial(ejercicios[nextEjercicio].codigo_estudiante)); // Actualizar código del estudiante
         setPuedeContinuar(false);
         setResultadoEstudiante('');
     };
 
-
-
     return (
         <Container>
-            {ejercicios.length == 0 ?
-            <Box
+            {ejercicios.length === 0 ?
+                <Box
                     sx={{
                         position: 'fixed',
                         top: 0,
@@ -101,47 +126,95 @@ function Ejercicios() {
                         alignItems: 'center', // Centrado vertical
                         backgroundColor: '#f0f0f0', // Color de fondo opcional
                     }}
-            >
-               { !flag ? <CircularProgress  size={50} /> : <Typography>No Hay Ejercicios creados</Typography>}
-            </Box>
-            :
-            <>
-                <Stack direction={'row'} justifyContent={'space-between'}>
-                    <Typography variant="h4" gutterBottom>
-                        Ejercicios
-                    </Typography>
-                    <IconButton
-                        sx={{
-                            width: '50px',
-                            height: '50px',
-                            borderRadius: '50%',
-                            padding: 0,
-                            backgroundColor: '#f0f0f0',
-                            '&:hover': {
-                                backgroundColor: '#e0e0e0',
-                            },
-                        }}
-                        onClick={() => navigate('/home')}
-                    >
-                        <GoHomeFill size={25} />
-                    </IconButton>
-                </Stack>
-                <Typography variant="body1" dangerouslySetInnerHTML={{ __html: ejercicios[ejercicioActual]?.descripcion }}>
-                    
-                </Typography>
-                <TerminalPyodide pyprompt={pyprompt} setPyprompt={setPyprompt} pyoutput={pyoutput} setPyoutput={setPyoutput}/>
-                <Grid container direction="column" spacing={2}>
-                    <Grid item xs={12} container justifyContent="flex-end">
-                        <Button
-                            variant="contained"
-                            disabled={ejercicioActual == ejercicios.length-1}
-                            onClick={handleSiguiente}
+                >
+                    {!flag ? <CircularProgress size={50} /> : <Typography>No Hay Ejercicios creados</Typography>}
+                </Box>
+                :
+                <>
+                    <Stack direction={'row'} justifyContent={'space-between'}>
+                        <Typography variant="h4" gutterBottom>
+                            Ejercicios
+                        </Typography>
+                        <IconButton
+                            sx={{
+                                width: '50px',
+                                height: '50px',
+                                borderRadius: '50%',
+                                padding: 0,
+                                backgroundColor: '#f0f0f0',
+                                '&:hover': {
+                                    backgroundColor: '#e0e0e0',
+                                },
+                            }}
+                            onClick={() => navigate('/home')}
                         >
-                            Siguiente
-                        </Button>
+                            <GoHomeFill size={25} />
+                        </IconButton>
+                    </Stack>
+                    <Typography variant="body1" dangerouslySetInnerHTML={{ __html: ejercicios[ejercicioActual]?.descripcion }}>
+                        
+                    </Typography>
+                    <AceEditor
+                        mode="python"
+                        theme="monokai"
+                        onChange={setCodigoEstudiante}
+                        name="codigoEstudiante"
+                        editorProps={{ $blockScrolling: true }}
+                        height="12em"
+                        width="100%"
+                        setOptions={{
+                            showLineNumbers: true,
+                        }}
+                        value={codigoEstudiante}
+                    />
+                    <br />
+                    <Grid container direction="column" spacing={2}>
+                        <Grid item xs={12} container justifyContent="flex-start">
+                            <Button variant="contained" onClick={handleRun}>
+                                Correr
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                multiline
+                                minRows={3}
+                                value={resultadoEstudiante}
+                                error={isError}
+                                InputProps={{
+                                    style: { color: isError ? 'red' : 'black' },
+                                }}
+                            />
+                        </Grid>
+
+                        {intentosFallidos >= 3 && intentosFallidos < 5 && (
+                            <Grid item xs={12}>
+                                <Typography variant="body1" color="error">
+                                    {ejercicios[ejercicioActual]?.ayuda_uno}
+                                </Typography>
+                            </Grid>
+                        )}
+
+                        {intentosFallidos >= 5 && (
+                            <Grid item xs={12}>
+                                <Typography variant="body1" color="error">
+                                    {ejercicios[ejercicioActual]?.ayuda_dos}
+                                </Typography>
+                            </Grid>
+                        )}
+
+                        <Grid item xs={12} container justifyContent="flex-end">
+                            <Button
+                                variant="contained"
+                                disabled={!puedeContinuar}
+                                onClick={handleSiguiente}
+                            >
+                                Siguiente
+                            </Button>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </>
+                </>
             }
         </Container>
     );
